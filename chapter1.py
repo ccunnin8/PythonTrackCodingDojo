ff13ce483c62a40c52c887b41cbdee1ec0bc3736
CoreyCunningham <coreyjjc@me.com>
2018-02-03 12:46:35 -0500

    multiples, sum, average

--- chapter1.py
+++ chapter1.py
@@ -34,3 +34,37 @@ Start with a list like this one: x = [19,2,54,-2,7,12,98,32,10,-3,6]. Sort your 
 # y = sorted(x)
 # y = y[0:len(y)/2]
 # print [y] + x[len(y):]
+
+"""
+Assignment: Multiples, Sum, Average
+This assignment has several parts. All of your code should be in one file that is well commented to indicate what each block of code is doing and which problem you are solving. Don't forget to test your code as you go!
+
+Multiples
+Part I - Write code that prints all the odd numbers from 1 to 1000. Use the for loop and don't use a list to do this exercise.
+
+Part II - Create another program that prints all the multiples of 5 from 5 to 1,000,000.
+
+Sum List
+Create a program that prints the sum of all the values in the list: a = [1, 2, 5, 10, 255, 3]
+
+Average List
+Create a program that prints the average of the values in the list: a = [1, 2, 5, 10, 255, 3]
+"""
+
+#multiples
+# for i in range(1,1000):
+#     if not i % 2 == 0:
+#         print i
+
+# for i in range(5,1000001):
+#     if i % 5 == 0:
+#         print i
+
+
+#sum list
+# a = [1, 2, 5, 10, 255, 3]
+# print sum(a)
+
+#average list
+# a = [1, 2, 5, 10, 255, 3]
+# print sum(a) / float(len(a))
